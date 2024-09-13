da3ed1c805b9070c3038f0720260082d4c3ef72d
jm609 <jaemin6093@gmail.com>
2024-09-13 16:01:52 +0900

    simple Quantum circuit with aersimulator, visualization

--- qi.py
+++ qi.py
@@ -1,8 +1,20 @@
 import qiskit
+qc = qiskit.QuantumCircuit(2)
+qc.h(0)
+qc.cx(0, 1)
+print(qc)
+qc.measure_all()
 
-qreg = qiskit.QuantumRegister(1, name='qreg')
-creg = qiskit.ClassicalRegister(1, name='creg')
-circ = qiskit.QuantumCircuit(qreg, creg)
-circ.x(qreg[0])
-circ.measure(qreg, creg)
-print(circ.draw())
+from qiskit_aer import AerSimulator
+from qiskit import transpile, assemble
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+simulator = AerSimulator()
+
+compiled_circuit = transpile(qc, simulator)
+result = simulator.run(compiled_circuit).result()
+counts = result.get_counts(qc)
+print(counts)
+
+plot_histogram(counts)
+plt.show()
