2687e544b86ece15fdff559332d24fa5ca0e2449
jm609 <jaemin6093@gmail.com>
2024-09-14 20:08:47 +0900

    superdense coding

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
-import random
 import cirq
 
-def make_quantum_teleportation_circuit(ranX, ranY):
-    circuit = cirq.Circuit()
-    msg, alice, bob= cirq.LineQubit.range(3)
-    # 엘리스와 밥 사이의 공유할 벨 상태 생성
-    circuit.append([cirq.H(alice), cirq.CNOT(alice, bob)])
-    # 메세지에 해당하는 임의의 상태 생성
-    circuit.append([cirq.X(msg)**ranX, cirq.Y(msg)**ranY])
-    # 메세지와 엘리스의 얽힌 큐비트에 대해 벨 측정
-    circuit.append([cirq.CNOT(msg, alice), cirq.H(msg)])
-    circuit.append(cirq.measure(msg, alice))
-    # 두 개의 고전 비트를 사용해 벨 측정 결과로부터 밥의 얽힌 큐비트에 원래 양자 메세지 복원
-    circuit.append([cirq.CNOT(alice, bob), cirq.CZ(msg, bob)])
-    return msg, circuit
+def bitstring(bits):
+    return ''.join('1' if e else '0' for e in bits)
 
-def main():
-    # 양자 순간 이동할 임의의 상태를 인코딩
-    ranX = random.random()
-    ranY = random.random()
-    msg, circuit = make_quantum_teleportation_circuit(ranX, ranY)
-    sim = cirq.Simulator()
-    message = sim.simulate(cirq.Circuit([cirq.X(msg)**ranX, cirq.Y(msg)**ranY]))
-    # 엘리스의 큐비트에 해당하는 블로흐 구 출력
-    b0X, b0Y, b0Z = cirq.bloch_vector_from_state_vector(
-        message.final_state_vector, 0)
-    print("x: ", round(b0X, 4),
-          "y: ", round(b0Y, 4),
-          "z: ", round(b0Z, 4))
-    print(circuit)
-    # 시뮬레이션의 최종 상태 기록
-    final_results = sim.simulate(circuit)  
-    # 밥의 큐비트에 해당하는 블로흐 구 출력
-    b2X, b2Y, b2Z = cirq.bloch_vector_from_state_vector(final_results.final_state_vector, 2)
-    print("x: ", round(b2X, 4),
-          "y: ", round(b2Y, 4),
-          "z: ", round(b2Z, 4))
-          
-if __name__ == '__main__':
-    main()
+qreg = [cirq.LineQubit(x) for x in range(2)]
+circ = cirq.Circuit()
+
+# 각 메세지별 연산 딕셔너리
+message = {"00": [], 
+"01": [cirq.X(qreg[0])],
+"10": [cirq.Z(qreg[0])],
+"11": [cirq.X(qreg[0]), cirq.Z(qreg[0])]}
+
+# 엘리스가 벨 쌍 생성
+circ.append(cirq.H(qreg[0]))
+circ.append(cirq.CNOT(qreg[0], qreg[1]))
+
+# 엘리스가 보낼 메세지 선택 및 인코딩
+m  = "01"
+print(m)
+circ.append(message[m])
+
+# 밥이 벨 기저로 측정
+circ.append(cirq.CNOT(qreg[0], qreg[1]))
+circ.append(cirq.H(qreg[0]))
+circ.append([cirq.measure(qreg[0]), cirq.measure(qreg[1])])
+
+# 양자회로 출력
+print(circ)
+
+# 양자회로 시물레이션
+sim = cirq.Simulator()
+res =sim.run(circ, repetitions=1)
+received_bits = res.measurements['q(0)'][0][0], res.measurements['q(1)'][0][0]
+
+print(bitstring(received_bits))
